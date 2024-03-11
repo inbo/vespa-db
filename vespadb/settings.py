f710b615ac785780785847d688d7d5574f74f1bc
Steven Gerrits <steven.gerrits94@gmail.com>
2024-03-11 11:02:27 +0000

    rearrange static and templates files

--- vespadb/settings.py
+++ vespadb/settings.py
@@ -109,7 +109,7 @@ ROOT_URLCONF = "vespadb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -161,7 +161,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static", "static"
 ]
 
 # Default primary key field type

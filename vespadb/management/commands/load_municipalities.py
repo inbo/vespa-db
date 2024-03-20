53c585f868ba2f531992a3f7a4be4d886d3c87ee
Steven Gerrits <steven.gerrits94@gmail.com>
2024-03-20 11:35:52 +0000

    fix linting

--- vespadb/management/commands/load_municipalities.py
+++ vespadb/management/commands/load_municipalities.py
@@ -1,13 +1,19 @@
 """Handle the load_municipalities command."""
-from django.core.management.base import BaseCommand
-import geopandas as gpd
-from vespadb.observations.models import Municipality
-import os
+
+from pathlib import Path
 from typing import Any
-from django.contrib.gis.geos import MultiPolygon, GEOSGeometry, Polygon
+
+import geopandas as gpd
+from django.contrib.gis.geos import GEOSGeometry, MultiPolygon, Polygon
+from django.core.management.base import BaseCommand
+
+from vespadb.observations.models import Municipality
+
 
 class Command(BaseCommand):
-    help = 'Load municipalities from a Shapefile into the database'
+    """Load municipalities from a Shapefile into the database."""
+
+    help = "Load municipalities from a Shapefile into the database"
 
     def handle(self, *args: Any, **kwargs: Any) -> None:
         """
@@ -17,24 +23,25 @@ class Command(BaseCommand):
             *args (Any): Variable length argument list.
             **kwargs (Any): Arbitrary keyword arguments.
 
-        Returns:
+        Returns
+        -------
             None
         """
         # Load the GeoDataFrame from the Shapefile
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        shp_path = os.path.join(script_dir, 'data/Refgem.shp')
+        script_dir = Path(__file__).parent
+        shp_path = script_dir / "data/Refgem.shp"
 
-        gdf = gpd.read_file(shp_path)
-        for index, row in gdf.iterrows():
-            nis_code = row['NISCODE']
-            name = row['NAAM']
-            geometry = GEOSGeometry(row['geometry'].wkt)
-            
+        gdf = gpd.read_file(str(shp_path))
+        for _, row in gdf.iterrows():
+            nis_code = row["NISCODE"]
+            name = row["NAAM"]
+            geometry = GEOSGeometry(row["geometry"].wkt)
+
             if isinstance(geometry, Polygon):
                 geometry = MultiPolygon(geometry)
 
             municipality, created = Municipality.objects.get_or_create(
-                name=name, nis_code=nis_code, defaults={'polygon': geometry}
+                name=name, nis_code=nis_code, defaults={"polygon": geometry}
             )
 
             if created:

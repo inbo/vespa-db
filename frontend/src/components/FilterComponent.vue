f84c8dd60480077f269b8583ee2fa78db3215865
Steven Gerrits <steven.gerrits94@gmail.com>
2024-03-27 19:58:00 +0000

    Add additional filters nesttype, neststatus

--- frontend/src/components/FilterComponent.vue
+++ frontend/src/components/FilterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col v-if="formattedMunicipalities.length > 0" cols="5" sm="3" md="3" lg="3" class="d-flex align-start">
+    <v-col v-if="formattedMunicipalities.length > 0" cols="2" class="d-flex align-start">
       <v-autocomplete v-model="selectedMunicipalities" :items="municipalities.length ? municipalities.map(municipality => ({
       title: municipality.name,
       value: municipality.id
@@ -8,11 +8,27 @@
         @change="emitFilterUpdate">
       </v-autocomplete>
     </v-col>
-    <v-col cols="5" sm="3" md="3" lg="3" class="d-flex align-start">
+    <v-col cols="2" class="d-flex align-start">
       <v-autocomplete v-model="selectedYears" :items="jaartallen" label="Selecteer jaartal(len)" multiple chips dense
         solo @change="emitFilterUpdate"></v-autocomplete>
     </v-col>
-    <v-col cols="2" sm="3" md="3" lg="3" class="d-flex align-start">
+    <v-col cols="2"class="d-flex align-start">
+      <v-autocomplete v-model="selectedNestType" :items="nestType.length ? nestType.map(nesttype => ({
+      title: nesttype.name,
+      value: nesttype.value
+    })) : []" item-text="title" item-value="value" label="Selecteer nesttype" multiple chips dense solo
+        @change="emitFilterUpdate">
+      </v-autocomplete>
+    </v-col>
+    <v-col cols="2" class="d-flex align-start">
+      <v-autocomplete v-model="selectedNestStatus" :items="nestStatus.length ? nestStatus.map(neststatus => ({
+      title: neststatus.name,
+      value: neststatus.value
+    })) : []" item-text="title" item-value="value" label="Selecteer neststatus" multiple chips dense solo
+        @change="emitFilterUpdate">
+      </v-autocomplete>
+    </v-col>
+    <v-col cols="1" class="d-flex align-start">
       <v-switch v-model="anbAreasActief" :label="`ANB Areas ${anbAreasActief ? 'Aan' : 'Uit'}`"
         :color="anbAreasActief ? '#4C7742' : ''" @change="emitFilterUpdate"></v-switch>
     </v-col>
@@ -29,12 +45,49 @@ export default {
       selectedMunicipalities: [],
       municipalities: [],
       selectedYears: [],
-      jaartallen: [2020, 2021, 2022, 2023],
+      selectedNestType: [],
+      selectedNestStatus: [],
+      jaartallen: [2020, 2021, 2022, 2023, 2024],
       anbAreasActief: false,
+      nestType: [
+        { name: 'AH - actief embryonaal nest', value: 'AH_actief_embryonaal_nest' },
+        { name: 'AH - actief primair nest', value: 'AH_actief_primair_nest' },
+        { name: 'AH - actief secundair nest', value: 'AH_actief_secundair_nest' },
+        { name: 'AH - inactief/leeg nest', value: 'AH_inactief_leeg_nest' },
+        { name: 'AH - potentieel nest (meer info nodig)', value: 'AH_potentieel_nest' },
+        { name: 'Nest andere soort', value: 'nest_andere_soort' },
+        { name: 'Geen nest (object, insect)', value: 'geen_nest' }
+      ],
+      nestStatus: [
+        { name: 'Uitgeroeid', value: 'eradicated' },
+        { name: 'Gereserveerd', value: 'reserved' },
+        { name: 'Open', value: 'open' }
+      ],
     };
   },
   watch: {
     selectedMunicipalities: {
+      handler() {
+        this.emitFilterUpdate();
+      },
+      deep: true,
+    },
+    selectedYears: {
+      handler() {
+        this.emitFilterUpdate();
+      },
+      deep: true,
+    },
+    anbAreasActief: function() {
+      this.emitFilterUpdate();
+    },
+    selectedNestType: {
+      handler() {
+        this.emitFilterUpdate();
+      },
+      deep: true,
+    },
+    selectedNestStatus: {
       handler() {
         this.emitFilterUpdate();
       },
@@ -67,7 +120,9 @@ export default {
       vespaStore.applyFilters({
         municipalities: this.selectedMunicipalities,
         years: this.selectedYears,
-        anbAreasActief: this.anbAreasActief
+        anbAreasActief: this.anbAreasActief,
+        nestType: this.selectedNestType,
+        nestStatus: this.selectedNestStatus,
       });
     },
   },

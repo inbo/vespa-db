0fb89dac85eb87f35697bfa7812050d059aafc53
Steven Gerrits <steven.gerrits94@gmail.com>
2024-04-04 12:13:59 +0000

    fix linting

--- vespadb/management/commands/assign_provinces_to_municipalities.py
+++ vespadb/management/commands/assign_provinces_to_municipalities.py
@@ -1,12 +1,19 @@
 """Assign provinces to municipalities."""
-from django.core.management.base import BaseCommand
-from vespadb.observations.models import Province, Municipality
+
 from typing import Any
 
+from django.core.management.base import BaseCommand
+
+from vespadb.observations.models import Municipality, Province
+
+
 class Command(BaseCommand):
-    help = 'Assign a province to each municipality based on their polygons'
+    """Assign provinces to municipalities."""
+
+    help = "Assign a province to each municipality based on their polygons"
 
     def handle(self, *args: Any, **options: Any) -> None:
+        """Assign a province to each municipality based on their polygons."""
         for municipality in Municipality.objects.all():
             # Vind de provincie(s) waarvan de polygon intersecteert met de polygon van de gemeente
             provinces = Province.objects.filter(polygon__intersects=municipality.polygon)
@@ -15,6 +22,10 @@ class Command(BaseCommand):
                 province = provinces.first()
                 municipality.province = province
                 municipality.save()
-                self.stdout.write(self.style.SUCCESS(f'Successfully assigned province {province.name} to municipality {municipality.name}'))
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Successfully assigned province {province.name} to municipality {municipality.name}"
+                    )
+                )
             else:
-                self.stdout.write(self.style.WARNING(f'No province found for municipality {municipality.name}'))
+                self.stdout.write(self.style.WARNING(f"No province found for municipality {municipality.name}"))

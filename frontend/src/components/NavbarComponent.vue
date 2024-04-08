5e9f03ffee8abfcdaae59f235a807a7869dfa115
stevegerrits <82342664+stevegerrits@users.noreply.github.com>
2024-04-08 19:10:32 +0200

    toggle table and map view (#44)

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -2,11 +2,21 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-success">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">VespaWatch Dashboard</a>
-      <div class="d-flex">
+      <div class="d-flex align-items-center">
+        <!-- View Mode Toggle -->
+        <div class="btn-group me-2" role="group">
+          <input type="radio" class="btn-check" name="viewMode" id="mapView" autocomplete="off" :checked="viewMode === 'map'" @change="setViewMode('map')">
+          <label class="btn btn-outline-light" for="mapView">Map</label>
+
+          <input type="radio" class="btn-check" name="viewMode" id="tableView" autocomplete="off" :checked="viewMode === 'table'" @change="setViewMode('table')">
+          <label class="btn btn-outline-light" for="tableView">Tabel</label>
+        </div>
+
+        <!-- User Login/Logout -->
         <span v-if="isLoggedIn" class="navbar-text">
           <div class="btn-group">
             <button type="button" class="btn btn-outline-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              {{username}}
+              {{ username }}
             </button>
             <ul class="dropdown-menu dropdown-menu-end">
               <li><button class="dropdown-item" @click="logout">Uitloggen</button></li>
@@ -30,7 +40,11 @@ export default {
     const vespaStore = useVespaStore();
     const isLoggedIn = computed(() => vespaStore.isLoggedIn);
     const username = computed(() => vespaStore.user.username);
+    const viewMode = computed(() => vespaStore.viewMode);
 
+    const setViewMode = (mode) => {
+            vespaStore.setViewMode(mode);
+    };
     const logout = async () => {
       await vespaStore.logout();
       router.push('/login');
@@ -38,7 +52,7 @@ export default {
     const navigateToChangePassword = () => {
       router.push({ name: 'ChangePassword' });
     };
-    return { isLoggedIn, username, logout, navigateToChangePassword };
+    return { isLoggedIn, username, logout, navigateToChangePassword, setViewMode, viewMode };
   },
 };
 </script>

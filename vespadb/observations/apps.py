4c111df3baf0dbe461f7b34fecbbf2ca77d83588
stevegerrits <82342664+stevegerrits@users.noreply.github.com>
2024-04-26 09:59:38 +0200

    set expired reservations to null (#59)
    * set expired reservations to null

--- vespadb/observations/apps.py
+++ vespadb/observations/apps.py
@@ -6,5 +6,9 @@ from django.apps import AppConfig
 class ObservationsConfig(AppConfig):
     """Config for observations app."""
 
-    default_auto_field = "django.db.models.BigAutoField"
     name = "vespadb.observations"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self) -> None:
+        """Import signals when the app is ready."""
+        import vespadb.observations.signals  # noqa: F401, PLC0415

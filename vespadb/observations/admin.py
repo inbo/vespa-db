df7c9e044fcf89c3aa2ef8be1e9721b5f365b7cb
Steven Gerrits <steven.gerrits94@gmail.com>
2024-05-06 12:08:55 +0000

    observation filters admin panel

--- vespadb/observations/admin.py
+++ vespadb/observations/admin.py
@@ -2,11 +2,62 @@
 
 from django.contrib import admin
 from django.contrib.gis import admin as gis_admin
-from vespadb.observations.models import Observation
+from vespadb.observations.models import (
+    Observation,
+    EradicationResultEnum,
+    ValidationStatusEnum,
+    NestHeightEnum,
+    NestSizeEnum,
+    NestLocationEnum,
+    NestTypeEnum,
+    EradicationMethodEnum,
+    EradicationAfterCareEnum,
+    Municipality,
+    Province,
+)
+from vespadb.users.models import VespaUser
 
 
 class ObservationAdmin(gis_admin.GISModelAdmin):
     """Admin class for Observation model."""
+
+    list_display = (
+        "id",
+        "observation_datetime",
+        "eradication_datetime",
+        "eradicator_name",
+        "wn_validation_status",
+        "nest_height",
+        "nest_size",
+        "nest_location",
+        "nest_type",
+        "eradication_result",
+        "municipality",
+        "province",
+        "reserved_by",
+        "created_by",
+        "modified_by",
+    )
+    list_filter = (
+        "observation_datetime",
+        "eradication_datetime",
+        "eradicator_name",
+        "wn_validation_status",
+        "nest_height",
+        "nest_size",
+        "nest_location",
+        "nest_type",
+        "eradication_result",
+        "municipality",
+        "province",
+        "reserved_by",
+        "created_by",
+        "modified_by",
+    )
+    search_fields = ("id", "eradicator_name", "observer_name")
+    filter_horizontal = ()
+    ordering = ("-observation_datetime",)
+    raw_id_fields = ("municipality", "province")
 
 
 admin.site.register(Observation, ObservationAdmin)

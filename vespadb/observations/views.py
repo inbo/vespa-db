30df18dc1eab8849a75cfa748b704e8912027d77
stevegerrits <82342664+stevegerrits@users.noreply.github.com>
2024-05-14 15:58:27 +0200

    Gs observations import endpoint (#70)
    * observations import endpoint

--- vespadb/observations/views.py
+++ vespadb/observations/views.py
@@ -1,13 +1,12 @@
 """Views for the observations app."""
 
 import csv
-import json
+import io
 import logging
 from typing import TYPE_CHECKING, Any
 
-from django.contrib.gis.db.models.functions import Transform
-from django.contrib.gis.geos import GEOSGeometry
-from django.core.cache import cache
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db import transaction
 from django.db.models import CharField, OuterRef, QuerySet, Subquery, Value
 from django.db.models.functions import Coalesce
 from django.http import HttpResponse, JsonResponse
@@ -19,6 +18,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import filters, status
 from rest_framework.decorators import action
+from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny, BasePermission, IsAdminUser, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -250,7 +250,7 @@ class ObservationsViewSet(ModelViewSet):
         })
 
     @method_decorator(ratelimit(key="ip", rate="15/m", method="GET", block=True))
-    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+    def retrieve_list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         """
         Handle requests for the list of observations with pagination.
 
@@ -279,66 +279,138 @@ class ObservationsViewSet(ModelViewSet):
     @method_decorator(ratelimit(key="ip", rate="15/m", method="GET", block=True))
     @action(detail=False, methods=["get"], url_path="dynamic-geojson")
     def geojson(self, request: Request) -> HttpResponse:
-        """Return GeoJSON data based on the request parameters."""
-        # Create a modified query dictionary excluding 'bbox'
-        query_params = request.GET.copy()
-        bbox_str = query_params.pop("bbox", None)
+        """
+        Bulk import observations.
 
-        # Sort parameters and create a cache key without 'bbox'
-        sorted_params = "&".join(sorted(f"{key}={value}" for key, value in query_params.items()))
-        cache_key = f"vespadb::{request.path}::{sorted_params}"
-        logger.info(f"Checking cache for {cache_key}")
+        Accepts a JSON list of observation objects or a CSV file with equivalent data, validates them, and inserts them into the database if they are valid. The expected JSON structure and CSV headers must include:
 
-        # Attempt to get cached data
-        cached_data = cache.get(cache_key)
-        if cached_data:
-            logger.info("Cache hit - Returning cached response")
-            return JsonResponse(cached_data, safe=False)
+        JSON example:
+        ```json
+        [{
+            "wn_id": 101,
+            "location": {"latitude": 50.8503, "longitude": 4.3517},
+            ...
+        }]
+        ```
 
-        bbox_str = request.GET.get("bbox")
-        if bbox_str:
-            try:
-                bbox_coords = list(map(float, bbox_str.split(",")))
-                if len(bbox_coords) == BBOX_LENGTH:
-                    xmin, ymin, xmax, ymax = bbox_coords
-                    bbox_wkt = f"POLYGON(({xmin} {ymin}, {xmin} {ymax}, {xmax} {ymax}, {xmax} {ymin}, {xmin} {ymin}))"
-                    bbox = GEOSGeometry(bbox_wkt, srid=4326)
-                else:
-                    return HttpResponse("Invalid bbox format", status=status.HTTP_400_BAD_REQUEST)
-            except ValueError:
-                return HttpResponse("Invalid bbox values", status=status.HTTP_400_BAD_REQUEST)
+        CSV headers must include: wn_id, latitude, longitude, etc.
+
+        Required fields include wn_id, location, species, and other fields depending on your specific validation rules.
+        """
+        # Check content type to parse data accordingly
+        content_type = request.content_type
+        if content_type == "application/json":
+            data = JSONParser().parse(request)
+        elif content_type == "text/csv":
+            file = request.FILES.get("file")
+            if not file:
+                return Response({"error": "CSV file is required."}, status=status.HTTP_400_BAD_REQUEST)
+            data = self.parse_csv(file)
         else:
-            bbox = None
+            return Response({"error": "Unsupported content type."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Apply filters
-        queryset = self.filter_queryset(self.get_queryset())
+        if not isinstance(data, list):
+            return Response({"error": "Expected a list of observation data"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if bbox:
-            queryset = queryset.filter(location__within=bbox)
+        valid_observations = []
+        errors = []
 
-        queryset = queryset.annotate(point=Transform("location", 4326))
+        # Validate each observation data using the ObservationSerializer
+        for item in data:
+            serializer = ObservationSerializer(data=item)
+            if serializer.is_valid():
+                valid_observations.append(serializer.validated_data)
+            else:
+                errors.append(serializer.errors)
 
-        features = [
-            {
-                "type": "Feature",
-                "properties": {"id": obs.id},
-                "geometry": json.loads(obs.point.geojson) if obs.point else None,
-            }
-            for obs in queryset
-        ]
-        geojson_response = {"type": "FeatureCollection", "features": features}
-        cache.set(cache_key, geojson_response, REDIS_CACHE_EXPIRATION)  # 24 hours
-        return JsonResponse(geojson_response)
+        if errors:
+            return Response({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Use Django's transaction to ensure atomicity of the bulk create operation
+        with transaction.atomic():
+            Observation.objects.bulk_create([Observation(**data) for data in valid_observations])
+
+        return Response(
+            {"message": f"Successfully imported {len(valid_observations)} observations."},
+            status=status.HTTP_201_CREATED,
+        )
+
+    def parse_csv(self, file: InMemoryUploadedFile) -> list[dict[str, Any]]:
+        """
+        Parse a CSV file to a list of dictionaries.
+
+        Each row in the CSV is converted to a dictionary with keys corresponding to the CSV column headers.
+        Assumes CSV contains headers that match the observation model fields directly and includes specific handling
+        for geographic coordinates which are converted to a 'location' dictionary suitable for use with serializers.
+
+        :param file: In-memory file object containing CSV data.
+        :return: List of dictionaries representing each row.
+        """
+        file.seek(0)
+        # Read and decode the file, ensuring compatibility with various CSV formatting
+        reader = csv.DictReader(io.StringIO(file.read().decode("utf-8")))
+        result: list[dict[str, Any]] = []
+        for row in reader:
+            # Convert string latitude and longitude to float and combine into a 'location' dictionary
+            if "latitude" in row and "longitude" in row:
+                try:
+                    latitude = float(row["latitude"])
+                    longitude = float(row["longitude"])
+                    row["location"] = {"latitude": latitude, "longitude": longitude}
+                except ValueError:
+                    continue  # Optionally handle or log errors for invalid data
+            # Add the modified row dictionary to the result list
+            result.append(row)
+        return result
 
     @action(detail=False, methods=["post"], permission_classes=[IsAdminUser])
     def bulk_import(self, request: Request) -> Response:
-        """Allow bulk import of observations for admin users only.
-
-        :param request: The request object.
-        :return: HTTP Response indicating the status of the operation.
         """
-        # Placeholder for bulk import logic.
-        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+        Bulk import observations.
+
+        Accepts a JSON list of observation objects, validates them, and inserts them into the database if they are valid. Each object should conform to the following structure:
+
+        ```json
+        {
+            "wn_id": 101,
+            "location": {"latitude": 50.8503, "longitude": 4.3517},
+            ...
+        }
+        ```
+
+        Required fields include wn_id, location, species, and other fields depending on your specific validation rules.
+        """
+        # Parse the incoming JSON data
+        data = JSONParser().parse(request)
+
+        if not isinstance(data, list):
+            return Response({"error": "Expected a list of observation data"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # This will store all valid observations to be bulk created
+        valid_observations = []
+        errors = []
+
+        # Validate each observation data using the ObservationSerializer
+        for item in data:
+            serializer = ObservationSerializer(data=item)
+            if serializer.is_valid():
+                valid_observations.append(serializer.validated_data)
+            else:
+                errors.append(serializer.errors)
+
+        if errors:
+            # If there are errors, return them with a 400 Bad Request
+            return Response({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Use Django's transaction to ensure atomicity of the bulk create operation
+        with transaction.atomic():
+            # Create all validated observations at once
+            Observation.objects.bulk_create([Observation(**data) for data in valid_observations])
+
+        return Response(
+            {"message": f"Successfully imported {len(valid_observations)} observations."},
+            status=status.HTTP_201_CREATED,
+        )
 
     @swagger_auto_schema(
         method="get",

d43fb894f8a37e090fc3207913142a6fc855524b
Steven Gerrits <steven.gerrits94@gmail.com>
2024-05-14 14:17:41 +0000

    fix: linting

--- vespadb/management/commands/load_waarnemingen_observations.py
+++ vespadb/management/commands/load_waarnemingen_observations.py
@@ -1,13 +1,19 @@
 """Load waarnemingen observations into the database."""
-from django.core.management.base import BaseCommand
-from vespadb.observations.tasks.observation_sync import fetch_and_update_observations
+
 from typing import Any
 
+from django.core.management.base import BaseCommand
+
+from vespadb.observations.tasks.observation_sync import fetch_and_update_observations
+
+
 class Command(BaseCommand):
-    help = 'Load waarnemingen observations into the database'
+    """Load waarnemingen observations into the database."""
+
+    help = "Load waarnemingen observations into the database"
 
     def handle(self, *args: Any, **options: Any) -> None:
         """Load waarnemingen observations into the database."""
         since_week = int(input("Enter the number of weeks back to load observations: "))
         fetch_and_update_observations(since_week=since_week)
-        self.stdout.write(self.style.SUCCESS('Observations loaded successfully'))
+        self.stdout.write(self.style.SUCCESS("Observations loaded successfully"))

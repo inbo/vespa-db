615a5b272504780a850313ac589f70332f4d0209
Steven Gerrits <steven.gerrits94@gmail.com>
2024-05-21 15:46:55 +0000

    fix entrypoint.sh and simplify dockercompose

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,12 @@
 # exit immediately if a command exits with a non-zero status
 set -e
 
-# Wait for the PostgreSQL database to be ready
-echo "Waiting for PostgreSQL database..."
-while ! pg_isready -h db -p 5432 -U vespauser; do
-  sleep 1
-done
-echo "Database is up!"
-
 # Migrate the database
 echo "Applying database migrations..."
 python manage.py migrate --noinput
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 # Load initial data if required
 echo "Loading municipalities, provinces and anb areas..."
@@ -24,3 +20,6 @@ echo "Assign provinces to municipalities..."
 python manage.py assign_provinces_to_municipalities
 
 exec /opt/vespadb-env/bin/poe "$@"
+
+# Start server
+echo "Starting server..."

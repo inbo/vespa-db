5efe671cfd2bf35a887cfdc1e7b61227d9a9f56f
stevegerrits <82342664+stevegerrits@users.noreply.github.com>
2024-06-08 13:07:55 +0200

    fix frontend deployment
    * fix frontend deployment
    
    * fix frontend dockerfile tag
    
    * remove dist and copy build steps

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+
+export default [
+  { languageOptions: { globals: globals.browser }},
+  ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      'vue/no-parsing-error': 'error',
+    }
+  }
+];
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vespa-db/',
+  base: './',
   plugins: [
     Vue({
       template: { transformAssetUrls }

64b054281a58aeadaac911609025017da678a169
Steven Gerrits <steven.gerrits94@gmail.com>
2024-06-09 07:20:36 +0000

    fix base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/vespa-db/',
   plugins: [
     Vue({
       template: { transformAssetUrls }

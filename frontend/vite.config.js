a9f3032a9e80af991da2699be863c80782f6e52b
Steven <steven@gssoftware.be>
2024-06-18 12:58:07 +0000

    use env var

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,53 +6,63 @@ import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // Utilities
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    Vue({
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    Vuetify(),
-    Components(),
-    ViteFonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-      custom: {
-        families: [
-          {
-            name: 'MaterialDesignIcons',
-            local: 'MaterialDesignIcons',
-            src: './src/assets/materialdesignicons-webfont.woff2',
-          },
-        ],
-        preload: true,
-      }
-    }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  // Set the second parameter to '' to load all .env files regardless of the mode
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/',
+    plugins: [
+      Vue({
+        template: { transformAssetUrls }
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+      Vuetify(),
+      Components(),
+      ViteFonts({
+        google: {
+          families: [{
+            name: 'Roboto',
+            styles: 'wght@100;300;400;500;700;900',
+          }],
+        },
+        custom: {
+          families: [
+            {
+              name: 'MaterialDesignIcons',
+              local: 'MaterialDesignIcons',
+              src: './src/assets/materialdesignicons-webfont.woff2',
+            },
+          ],
+          preload: true,
+        }
+      }),
     ],
-  },
-  server: {
-    port: 3000,
-  },
+    define: {
+      'process.env': {
+        VITE_APP_API_URL: JSON.stringify(env.VITE_APP_API_URL),
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+      ],
+    },
+    server: {
+      port: 3000,
+    },
+  }
 })

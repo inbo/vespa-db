02071668f1cfbfa3a56e26b189263cfcdb106d34
Steven <steven@gssoftware.be>
2024-06-20 14:25:44 +0200

    env var fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,7 @@ import { defineConfig, loadEnv } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode
-  const env = loadEnv(mode, process.cwd(), '')
-
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return {
     base: '/',
     plugins: [
@@ -41,11 +40,9 @@ export default defineConfig(({ mode }) => {
         }
       }),
     ],
-    define: {
-      'process.env': {
-        VITE_APP_API_URL: JSON.stringify(process.env.VITE_APP_API_URL || env.VITE_APP_API_URL),
-      }
-    },
+    // define: {
+    //   VITE_APP_API_URL: `"${process.env.VITE_APP_API_URL}"`
+    // },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))

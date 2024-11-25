27e2d25e4a7ed4af113c5a842449606f10c3bbbd
Steven Gerrits <steven.gerrits94@gmail.com>
2024-11-25 12:06:47 +0000

    fix: usertype system user get or create

--- vespadb/users/utils.py
+++ vespadb/users/utils.py
@@ -5,9 +5,12 @@ from typing import Literal, cast
 from vespadb.users.models import UserType, VespaUser
 
 
-def get_system_user(user_type: Literal[UserType.SYNC, UserType.IMPORT]) -> VespaUser:
-    """Get System Users based on usertype."""
+def get_system_user(user_type: Literal[UserType.SYNC]) -> VespaUser:
+    """Get the system user specifically for SYNC."""
+    if user_type != UserType.SYNC:
+        raise ValueError("This function only supports UserType.SYNC.")
+
     system_user, _ = VespaUser.objects.get_or_create(
-        username=user_type.value, first_name="vespadb", defaults={"user_type": user_type.value}
+        username="sync", defaults={"user_type": UserType.SYNC.value}
     )
     return cast(VespaUser, system_user)  # make mypy happy

50b9dc5f9a6d0a40a740f18c78131a96b8ccfe01
Steven Gerrits <steven.gerrits94@gmail.com>
2025-02-11 11:15:01 +0000

    feb-r3

--- vespadb/users/urls.py
+++ vespadb/users/urls.py
@@ -12,7 +12,7 @@ router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("auth-check", AuthCheck.as_view(), name="auth_check"),
+    path("auth-check/", AuthCheck.as_view(), name="auth_check"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("change-password/", ChangePasswordView.as_view(), name="change_password"),

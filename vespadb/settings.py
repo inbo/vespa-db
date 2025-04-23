8f0d72e161e4b5287e7f138ee65be874a95b5c4a
mainlyIt <gs.software.info@gmail.com>
2025-04-23 20:37:08 +0200

    prewarm always (#398)

--- vespadb/settings.py
+++ vespadb/settings.py
@@ -132,7 +132,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     'prewarm-geojson-cache': {
         'task': "vespadb.observations.tasks.generate_geojson_task",
-        'schedule': crontab(minute='*/13', hour='9-17'),
+        'schedule': crontab(minute='*/13'),  # Geen restrictie meer op uren
         'args': ({
             'visible': 'true',
             'min_observation_datetime': '2024-04-01T00:00:00+02:00'

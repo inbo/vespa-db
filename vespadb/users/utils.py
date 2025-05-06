0350798418ca8b82efd75d392bce88792c231bd3
Steven Gerrits <steven.gerrits94@gmail.com>
2025-05-06 18:15:08 +0200

    fix import

--- vespadb/users/utils.py
+++ vespadb/users/utils.py
@@ -14,3 +14,14 @@ def get_system_user(user_type: Literal[UserType.SYNC]) -> VespaUser:
         username="sync", defaults={"user_type": UserType.SYNC.value}
     )
     return cast(VespaUser, system_user)  # make mypy happy
+
+
+def get_import_user(user_type: Literal[UserType.IMPORT]) -> VespaUser:
+    """Get the system user specifically for SYNC."""
+    if user_type != UserType.IMPORT:
+        raise ValueError("This function only supports UserType.IMPORT.")
+
+    import_user, _ = VespaUser.objects.get_or_create(
+        username="import", defaults={"user_type": UserType.IMPORT.value}
+    )
+    return cast(VespaUser, import_user)  # make mypy happy
